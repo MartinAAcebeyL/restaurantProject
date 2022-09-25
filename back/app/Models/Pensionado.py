0027a0b6c923202a69a2b7e4f0d2fa3d3e5df8c9
MartinAAcebeyL <martinaacbyl2000@gmail.com>
2022-09-25 10:50:54 -0400

    faker agregado

--- back/app/Models/Pensionado.py
+++ back/app/Models/Pensionado.py
@@ -1,25 +1,43 @@
 from . import db
+from . import fake
+from . import phone_number
+
 from sqlalchemy import Enum
 from datetime import datetime
 from sqlalchemy.event import listen
-
+import random
 class Pensionado(db.Model):
     __tablename__ = "pensionados"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20), nullable = False, unique = True)
-    phone = db.Column(db.String(10), nullable = False, unique = True)
+    name = db.Column(db.String(50), nullable = False, unique = True)
+    phone = db.Column(db.String(40), nullable = False, unique = True)
     email = db.Column(db.String(80), nullable = False, unique = True)
     sex = db.Column(db.Enum("M", "F", "O"), nullable=False, server_default="M")
     resgistred = db.Column(db.DateTime(), nullable=False, default= datetime.now())
 
-
-# def insertar_registros(*args, **kwargs):
-#     db.session.add(
-#         Pensionado(name='titulo 1',email="descripcion 1", phone="descripcion 1")
-#     )
+    @classmethod
+    def create(cls, name, phone, email, sex):
+        return Pensionado(name=name, phone=phone, email=email, sex=sex)
     
+    def save(self):
+        try:
+            db.session.add(self)
+            db.session.commit()
+            return True
+        except:
+            return False
 
-#     db.session.commit()
+def insertar_registros(*args, **kwargs):
+    for i in range(5):
+        name = fake.name()
+        phone = fake.phone_number()
+        email = fake.email()
+        sex = random.choice(['M', 'F'])
+        print(name, phone, email, sex)
+        pensionado = Pensionado.create(name=name, phone=phone, email=email, sex=sex)
+        if not pensionado.save():
+            print(f'{i} error, no se introdujo a ls BD')
 
-# listen(Pensionado.__table__, "after_create", insertar_registros)
+
+listen(Pensionado.__table__, "after_create", insertar_registros)
--- back/app/Models/__init__.py
+++ back/app/Models/__init__.py
@@ -1,3 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
+from faker import Faker
+from faker.providers import phone_number
 
 db = SQLAlchemy()
+Faker.seed(50)
+fake = Faker()

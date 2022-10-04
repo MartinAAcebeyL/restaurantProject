11de3944522901eb12bd91cb0c47949bf93d47c0
MartinAAcebeyL <martinaacbyl2000@gmail.com>
2022-10-03 23:54:18 -0400

    migraciones incorporado

--- back/app/__init__.py
+++ back/app/__init__.py
@@ -1,21 +1,23 @@
 from flask import Flask
+from flask_migrate import Migrate
 
 from .Models import db
 from .Models.Pensionado import Pensionado
 
 from .Routes.Pensionado import api
 
-
 app = Flask(__name__)
+db.init_app(app)
 
+migrate = Migrate(app, db)
 
 def create_app(config):
-    
+
     app.config.from_object(config)
     app.register_blueprint(api)
 
     with app.app_context():
-        db.init_app(app)
         db.create_all()
+
 
     return app
